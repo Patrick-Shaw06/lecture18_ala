82b4cf1265204578ac14347fd848fa9698e1a0ca
Patrick-Shaw06 <pshaw4@nd.edu>
2025-03-24 16:21:03 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,215 @@
+def playHalf(note):
+    music.play(music.tone_playable(note, music.beat(BeatFraction.WHOLE)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note, music.beat(BeatFraction.QUARTER)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note, music.beat(BeatFraction.EIGHTH)), music.PlaybackMode.UNTIL_DONE)
+    music.rest(music.beat(BeatFraction.EIGHTH))
+
+def playQuarter(note):
+    music.play(music.tone_playable(note, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note, music.beat(BeatFraction.QUARTER)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note, music.beat(BeatFraction.EIGHTH)), music.PlaybackMode.UNTIL_DONE)
+    music.rest(music.beat(BeatFraction.EIGHTH))
+
+def playTriplet(note1, note2, note3):
+    music.play(music.tone_playable(note1, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note1, music.beat(BeatFraction.EIGHTH)), music.PlaybackMode.UNTIL_DONE)
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play(music.tone_playable(note2, music.beat(BeatFraction.HALF)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note2, music.beat(BeatFraction.EIGHTH)), music.PlaybackMode.UNTIL_DONE)
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play(music.tone_playable(note3, music.beat(BeatFraction.QUARTER)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note3, music.beat(BeatFraction.EIGHTH)), music.PlaybackMode.UNTIL_DONE)
+    music.rest(music.beat(BeatFraction.EIGHTH))
+
+def playEighth(note):
+    music.play(music.tone_playable(note, music.beat(BeatFraction.QUARTER)), music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(note, music.beat(BeatFraction.EIGHTH)), music.PlaybackMode.UNTIL_DONE)
+    music.rest(music.beat(BeatFraction.EIGHTH))
+
+def restWhole():
+    music.rest(music.beat(BeatFraction.BREVE))
+
+def restHalf():
+    music.rest(music.beat(BeatFraction.DOUBLE))
+
+def restQuarter():
+    music.rest(music.beat(BeatFraction.WHOLE))
+
+def restEighth():
+    music.rest(music.beat(BeatFraction.HALF))
+
+def on_button_pressed_a():
+    music.set_tempo(140)
+    
+    # restWhole()
+    # restWhole()
+    # restWhole()
+    restHalf()
+    for i in range(2):
+        restEighth()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        restHalf()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.B3)
+        restHalf()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        restQuarter()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        restEighth()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.FSHARP)
+        playEighth(Note.GSHARP)
+        playEighth(Note.FSHARP)
+        restEighth()
+    restHalf()
+
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def on_forever():
-    pass
+    basic.show_leds("""
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . . .
+        # . . . .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # . . . #
+        . # . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # . . # .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # # # # .
+        # . . . .
+        # # # . .
+        # . . . .
+        # # # # .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        """)
+    pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,220 @@
+function playHalf(note: number) {
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+}
+
+function playQuarter(note: number) {
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+}
+
+function playTriplet(note1: number, note2: number, note3: number) {
+    music.play(music.tonePlayable(note1, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note1, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(note2, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note2, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(note3, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note3, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+}
+
+function playEighth(note: number) {
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(note, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+}
+
+function restWhole() {
+    music.rest(music.beat(BeatFraction.Breve))
+}
+
+function restHalf() {
+    music.rest(music.beat(BeatFraction.Double))
+}
+
+function restQuarter() {
+    music.rest(music.beat(BeatFraction.Whole))
+}
+
+function restEighth() {
+    music.rest(music.beat(BeatFraction.Half))
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    music.setTempo(140)
+    //  restWhole()
+    //  restWhole()
+    //  restWhole()
+    restHalf()
+    for (let i = 0; i < 2; i++) {
+        restEighth()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        restHalf()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.B3)
+        restHalf()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.E)
+        restQuarter()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        restEighth()
+        playEighth(Note.E)
+        playEighth(Note.E)
+        playEighth(Note.FSharp)
+        playEighth(Note.GSharp)
+        playEighth(Note.FSharp)
+        restEighth()
+    }
+    restHalf()
+})
 basic.forever(function on_forever() {
-    
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . . .
+        # . . . .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # . . # .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # . .
+        # . . . .
+        # # # # .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    pause(1000)
 })
